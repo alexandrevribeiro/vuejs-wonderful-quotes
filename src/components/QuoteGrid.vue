71e6032fd8b9bddaa24aca145e87b80c7b993b57
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-06-15 19:24:03 -0300

    Deleting quote feature

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <app-quote v-for="quote in quotes">
+        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">
             {{ quote }}
         </app-quote>
     </div>
@@ -14,6 +14,11 @@
         props: ['quotes'],
         components: {
             appQuote: Quote
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
         }
     }
 </script>
